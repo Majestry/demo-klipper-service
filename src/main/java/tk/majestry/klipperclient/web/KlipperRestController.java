ef01048ab7355b204af44be04b2339376096e938
Maksym Kulakivskyi <maksym.kulakivskyi@technipfmc.com>
2023-05-11 18:17:23 +0200

    Added demo feign staff

--- src/main/java/tk/majestry/klipperclient/web/KlipperRestController.java
+++ src/main/java/tk/majestry/klipperclient/web/KlipperRestController.java
@@ -2,20 +2,26 @@ package tk.majestry.klipperclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ThreadLocalRandom;
+import tk.majestry.KlipperResponse;
 
 @RestController
-public class KlipperRestController {
+public class KlipperRestController
+{
 
     @Value("${application.version}")
     private String appVersion;
 
     @GetMapping(path = "/hello")
-    public String getHello(@RequestParam(name = "name") String name) {
-        var responseData = "I'm a new version here, %s! My version is %s";
-        return String.format(responseData, name, appVersion);
+    public KlipperResponse getHello(@RequestParam(name = "name") String name,
+                                    @RequestHeader("X-Custom-Header") String customHeader)
+    {
+        KlipperResponse klipperResponse = new KlipperResponse();
+        klipperResponse.setName(name);
+        klipperResponse.setVersion(appVersion);
+        klipperResponse.setHeaderValue(customHeader);
+        return klipperResponse;
     }
 }
